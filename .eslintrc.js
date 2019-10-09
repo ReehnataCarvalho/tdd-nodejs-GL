2e2fa0deb434d79594584cac641eec4ab1448cc4
ReehnataCarvalho <reehnataoliveira592@gmail.com>
2019-10-09 11:50:08 -0300

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "extends": "airbnb-bsae",
+  "plugins": [
+    "import"
+  ],
+  "rules": {
+      "no-console": "off",
+      "no-unused-vars": "off",
+      "max-lend": 0
+
+  }
+};
